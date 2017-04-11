e4bf749c62e3cbfb1437339d0db75520d2848440
Anthony Milstead <Anthony Milstead>
2017-04-11 09:34:20 -0400

    first commit

---
+++ MyGuessingGame/Controllers/GuessingGameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyGuessingGame.Controllers
+{
+    public class GuessingGameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyGuessingGame/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyGuessingGame.MvcApplication" Language="C#" %>
