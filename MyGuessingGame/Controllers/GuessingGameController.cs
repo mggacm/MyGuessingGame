4122e0b445aaaffdd6de3471af0869e613516ace
Anthony Milstead <Anthony Milstead>
2017-04-11 10:30:39 -0400

    Adding to the Index

--- MyGuessingGame/Controllers/GuessingGameController.cs
+++ MyGuessingGame/Controllers/GuessingGameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyGuessingGame.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,18 @@ namespace MyGuessingGame.Controllers
     {
         public ActionResult Index()
         {
+            Session["Answer"] = new Random().Next(1, 10);   //Call-Name[string name] = new Random().Next(starting num, ending num);
+
             return View();
         }
+        
+        //Post
+        [HttpPost]
+        public ActionResult Index(GuessingGameModel model)
+        {
+
+            return View();
+        }
+
     }
 }
---
+++ MyGuessingGame/Models/GuessingGameModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyGuessingGame.Models
+{
+    public class GuessingGameModel 
+    {
+        
+        
+        public string PlayerName { get; set; }
+        public int Guess { get; set; }
+
+    }
+}
